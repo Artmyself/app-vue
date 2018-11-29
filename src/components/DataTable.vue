34afb5da522e3cb1c548075e5f439c9a2d6901db
Prabhu Chaudhary <prabhuchaudhary@MacBook-Pro-Prabhu.local>
2018-11-29 17:02:22 +0545

    initial commit

---
+++ src/components/DataTable.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+    {{data in datas}}
+    <table class="table" id="datatable">
+        <thead class="table-light-bg">
+            <th scope="col">Sn</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Address</th>
+        </thead>
+        <tbody>
+            <tr v-for="(item,index) in datas" :key="index">
+                <td>{{index+1}}</td>
+                <td>{{item.fname}}</td>
+                <td>{{item.lname}}</td>
+                <td>{{item.eaddress}}</td>
+                <td>{{item.add}}</td>
+                <td>{{item.checkedVechicle}}</td>
+                <td>{{item.location}}</td>
+                <td>{{item.payment}}</td>
+                <div class="inline-button-block">
+                    <button @click="editData(item)">Edit</button>
+                    <button @click="deleteData(item)">Delete</button>
+                </div>
+    </table>
+
+</div>
+</template>
+
+<script lang="ts">
+import {
+    Component,
+    Prop,
+    Vue,
+    Watch
+} from "vue-property-decorator";
+@Component({
+
+})
+export default class DataTable extends Vue {
+    //Edit Component Visiblity
+    editVisible: boolean;
+    @Prop()
+    public data!: object;
+
+    datas: any[];
+    rowData: object;
+    constructor() {
+        super()
+        this.editVisible = false;
+        this.datas = [];
+        this.rowData = {};
+    }
+    //Watches
+    @Watch("data")
+    onDataChange() {
+        this.datas.push(this.data);
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,138 @@
 <template>
-  <div class="home">
+<div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="checkoutform">
+        <form @submit.prevent="save">
+            <h3>Billing address</h3>First Name :
+            <input type="text" v-model="fname">
+            <br>Last Name :
+            <input type="text" v-model="lname">
+            <hr>E-mail:
+            <input type="email" v-model="eaddress">
+            <br>Address :
+            <input type="text" v-model="add">
+            <hr>
+            <input type="checkbox" value="Bike" v-model="checkedVechicle"> I have a bike
+            <input type="checkbox" value="Car" v-model="checkedVechicle"> I have a car
+            <br>
+
+            <label>Location</label>
+            <select v-model="location">
+            <option disabled value="">Please select one</option>
+            <option :value="'ktm'">Kathmandu</option>
+            <option :value="'pkr'">Pokhara</option>
+            <option :value="'chi'">Chitwan</option>
+            </select>
+            <hr>
+            <h3>Payment</h3>
+            <input type="radio" value="creditcard" v-model="payment"> Credit card
+            <input type="radio" value="debitcard" v-model="payment"> Debit card
+            <input type="radio" value="paypal" v-model="payment"> PayPal
+            <hr>
+
+            <button type="submit">Submit</button>
+            <button @click="reset()">Reset</button>
+
+        </form>
+    </div>
+    <datatable :data="data"></datatable>
+</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {
+    Component,
+    Prop,
+    Vue,
+    Watch
+} from "vue-property-decorator";
+import DataTable from "./DataTable.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        DataTable
+    }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    fname: String;
+    lname: String;
+    eaddress: String;
+    add: String;
+    checkedVechicle: any;
+    location: any;
+    payment: String;
+
+    firstname: String;
+    lastname: String;
+    emailaddress: String;
+    address: String;
+    checkVechicle: any;
+    llocation: any;
+    pay: String;
+
+    data: object;
+    constructor() {
+        super(); // super used to grab the features of the vue
+        this.fname = "";
+        this.lname = "";
+        this.eaddress = "";
+        this.add = "";
+        this.checkedVechicle = "";
+        this.location = "";
+        this.payment = "";
+
+        this.firstname = "";
+        this.lastname = "";
+        this.emailaddress = "";
+        this.checkVechicle = "";
+        this.address = "";
+        this.llocation = "";
+        this.pay = "";
+
+        this.data = {};
+    }
+    submit() {
+        if (
+            this.fname &&
+            this.lname &&
+            this.eaddress &&
+            this.add &&
+            this.checkedVechicle &&
+            this.location &&
+            this.payment
+        ) {
+            this.data = {
+                firstname: this.fname,
+                lastname: this.lname,
+                emailaddress: this.eaddress,
+                address: this.add,
+                checkVechicle: this.checkedVechicle,
+                llocation: this.location,
+                pay: this.payment
+            };
+            // alert(this.data);
+            this.clear();
+        } else {
+            alert("Fields cannot be empty");
+        }
+    }
+    clear() {
+        this.fname = "";
+        this.lname = "";
+        this.emailaddress = "";
+        this.address = "";
+        this.checkedVechicle = "";
+        this.location = "";
+        this.payment = "";
+    }
+    reset() {
+        this.fname = "";
+        this.lname = "";
+        this.eaddress = "";
+        this.add = "";
+        this.checkedVechicle = "";
+        this.location = "";
+        this.payment = "";
+    }
+}
 </script>
